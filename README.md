1d1d836ed85fb8f5f96dfd03fd76895907e25112
surrealwaffle <aomaniac@gmail.com>
2020-04-13 15:02:23 -0700

    Clean, fresh start

--- README.md
+++
@@ -1,4 +0,0 @@
-thesurrealwaffle
-================
-
-primitive stages of auto-tailored pathfinding in halo; touches reverse engineering, 
--- surreal_waffle/d3dproxy/stdafx.h
+++
@@ -1,8 +0,0 @@
-#ifndef STDAFX_H
-#define STDAFX_H
-
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#include <d3d9.h>
-
-#endif // STDAFX_H
